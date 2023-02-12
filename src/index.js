848a1d32a3ee5c070d6597d35b5d0a1f6ed9dcd3
Burak Yauhen <burakyauhen@gmail.com>
2023-02-12 12:24:36 +0300

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const config1 = [['(', ')']];
+  const config2 = [['(', ')'], ['[', ']']];
+  const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+  const config4 = [['|', '|']];
+  const config5 = [['(', ')'], ['|', '|']];
+  const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+  const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+  let arr = [];
+    let map = new Map(bracketsConfig);
+
+    for (let i = 0; i < str.length; i++) {
+        if (arr.length === 0) {
+            arr.push(str[i]);
+            continue;
+        } 
+        if (map.get(arr[arr.length - 1]) === str[i]) {
+            arr.pop(arr.length - 1);
+        } else arr.push(str[i]);
+    }
+
+    return (arr.length === 0) ? true: false;
 }
